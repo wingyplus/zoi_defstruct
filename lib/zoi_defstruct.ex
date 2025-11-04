d3507f7093c81b614020e4d8e32e3856a91efb30
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2025-11-04 22:23:37 +0700

    Generate typespec and add moduledoc
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- lib/zoi_defstruct.ex
+++ lib/zoi_defstruct.ex
@@ -1,29 +1,46 @@
 defmodule ZoiDefstruct do
   @moduledoc """
-  `ZoiDefstruct` help you define from `Zoi` object schema.
+  `ZoiDefstruct` helps you define a struct from a `Zoi` object schema.
 
-  ## Examples
+  ## Getting Started
+
+  Once you have a schema defined in the module, call `use ZoiDefstruct` and then
+  call `defstructure/1` to generate a struct:
 
       defmodule Person do
         use ZoiDefstruct
-      
-        @schema Zoi.object(%{name: Zoi.string(), age: Zoi.integer()})
-      
+
+        @schema Zoi.object(%{
+                  name: Zoi.string(),
+                  age: Zoi.integer()
+                })
+
         defstructure(@schema)
       end
-      
-      %Person{name: "hello"}
+
+      # Usage
+      person = %Person{name: "Alice", age: 30}
+
+  ## What gets generated
+
+  When `defstructure/1` is called, the macro generates:
+
+  - A struct definition via `defstruct` with fields from the schema.
+  - Default values for any fields defined with `Zoi.default/2`.
+  - Enforced keys (via `@enforce_keys`) for required fields that don't have default values.
+  - Generate the Typespec type `t()` from the schema.
   """
 
   alias Zoi.Types.Object, as: ZO
   alias Zoi.Types.Default, as: ZD
 
   @doc """
-  Define a struct from Zoi schema.
+  Define a struct from `Zoi` schema.
   """
   defmacro defstructure(schema) do
-    quote location: :keep do
-      schema = unquote(schema)
+    quote location: :keep, bind_quoted: [schema: schema] do
+      @type t :: unquote(Zoi.type_spec(schema))
+
       enforce_keys = ZoiDefstruct.struct_enforce_keys(schema)
 
       if enforce_keys != [] do
